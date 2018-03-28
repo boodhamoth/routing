66cb96a6469b3dabd845c51383c262728908cc67
dharanigovindaraju <dharanig13@gmail.com>
2018-03-28 15:11:57 +0530

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import{ RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'Page1', component:Page1Component},
+      {path:'',pathMatch:'full', redirectTo:'Page1'},
+      {path:'Page2', component:Page2Component}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
